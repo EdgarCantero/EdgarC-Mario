8f454ef08703dfefc7bf91f4228a71a9cf4535f4
Edgar Cantero <cantero098@gmail.com>
2014-12-01 10:31:50 -0800

    still working on it

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -17,7 +17,8 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        {name: "Edgar01", type: "tmx", src: "data/map/Edgarlv1.tmx"}
+        {name: "Edgar01", type: "tmx", src: "data/map/Edgarlv1.tmx"},
+        {name: "Edgar02", type: "tmx", src: "data/map/Edgarlv2.tmx"}
 
 	/* Background music. 
 	 * @example
